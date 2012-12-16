65a6a2e28736fef3d73805845d99d0bdab35bfab
Jordan Ranson <jordan@jordanranson.com>
2012-12-16 01:50:10 -0700

    Added game object manager

---
+++ vectorEngine2d/gameObjectManager.js
@@ -0,0 +1,12 @@
+var GameObjectManager = function(game) {
+	this.game = game;
+	this.gameObjects = [];
+};
+
+GameObjectManager.prototype.update = function() {
+
+};
+
+GameObjectManager.prototype.draw = function() {
+
+};
