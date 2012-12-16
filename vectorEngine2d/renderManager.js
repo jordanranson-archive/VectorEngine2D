227dc5891fa9d4d9c80c8aa59d7f8428cf1d3a8b
Jordan Ranson <jordan@jordanranson.com>
2012-12-16 01:04:09 -0700

    Added render manager for drawing to canvas

---
+++ vectorEngine2d/renderManager.js
@@ -0,0 +1,51 @@
+var RenderManager = function(canvas) {
+	this.canvas = document.getElementById(canvas);
+	this.context = this.canvas.getContext("2d");
+};
+
+RenderManager.prototype.clear = function() {
+	this.context.save();
+
+	// Use the identity matrix while clearing the canvas
+	this.context.setTransform(1, 0, 0, 1, 0, 0);
+	this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+	// Restore the transform
+	this.context.restore();
+};
+
+RenderManager.prototype.drawLine = function(x1, y1, x2, y2, strokeColor, strokeWidth) { 
+	this.context.beginPath();
+	this.context.moveTo(x1, y1);
+	this.context.lineTo(x2, y2);
+	this.context.lineWidth = strokeWidth;
+	this.context.strokeStyle = strokeColor;
+	this.context.stroke();
+};
+
+RenderManager.prototype.drawCircle = function(cx, cy, r, strokeColor, strokeWidth, fillColor) { 
+	this.context.beginPath();
+	this.context.arc(cx, cy, r, 0, 2 * Math.PI, false);
+	this.context.fillStyle = fillColor;
+	this.context.fill();
+	this.context.lineWidth = strokeWidth;
+	this.context.strokeStyle = strokeColor;
+	this.context.stroke();
+};
+
+RenderManager.prototype.drawRectangle = function(x, y, width, height, strokeColor, strokeWidth, fillColor) { 
+	this.context.beginPath();
+	this.context.rect(x, y, width, height);
+	this.context.fillStyle = fillColor;
+	this.context.fill();
+	this.context.lineWidth = strokeWidth;
+	this.context.strokeStyle = strokeColor;
+	this.context.stroke();
+};
+
+RenderManager.prototype.drawText = function(x, y, fillColor, fontStyle, textAlign, text) { 
+	this.context.font = fontStyle;
+	this.context.textAlign = textAlign;
+	this.context.fillStyle = fillColor;
+	this.context.fillText(text, x, y);
+};
