d43b3f7d3be15a91d2975a05e1609c8aef2fe4b3
Jordan Ranson <jordan@jordanranson.com>
2012-12-16 01:04:22 -0700

    Added input manager for getting keyboard input

---
+++ vectorEngine2d/inputManager.js
@@ -0,0 +1,50 @@
+var InputManager = function(keyMap) {
+	var defaultKeyMap = {
+		up: Key.upArrow,
+		down: Key.downArrow,
+		forward: Key.rightArrow,
+		backward: Key.leftArrow,
+		jump: Key.space,
+		action1: Key.z,
+		action2: Key.x,
+		action3: Key.c,
+		action4: Key.a,
+		action5: Key.s,
+		left: Key.leftArrow,
+		right: Key.rightArrow,
+		accept: Key.enter,
+		cancel: Key.escape,
+		pause: Key.escape
+	};
+	
+	var _this = this;
+	this.keyEvents = {};
+	this.keyAction = Util.extendParams(defaultKeyMap, keyMap);
+	this.keysPressed = {};
+	
+	this.keydown = function(e) {
+		this.keysPressed[e.keyCode] = true;
+		if(typeof(this.keyEvents[e.keyCode]) == "function") {
+			this.keyEvents[e.keyCode]();
+		}
+	};
+
+	this.keyup = function(e) {
+		delete this.keysPressed[e.keyCode];
+	};
+	
+	window.addEventListener('keyup', function(e) { _this.keyup(e); }, false);
+	window.addEventListener('keydown', function(e) { _this.keydown(e); }, false);
+};
+
+InputManager.prototype.isKeyDown = function(keyCode) {
+	return this.keysPressed[keyCode];
+};
+
+InputManager.prototype.addKeyEvent = function(keyCode, callback) {
+	this.keyEvents[keyCode] = callback;
+};
+
+InputManager.prototype.removeKeyEvent = function(keyCode) {
+	delete this.keyEvents[keyCode];
+};
