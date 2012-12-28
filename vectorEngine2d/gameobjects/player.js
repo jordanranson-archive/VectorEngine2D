c0648d2cc92b3a3104869e3eaa31583088f4cd5f
Jordan Ranson <jordan@jordanranson.com>
2012-12-28 04:21:29 -0700

    Optimized collision loop, added speed up

--- vectorEngine2d/gameobjects/player.js
+++ vectorEngine2d/gameobjects/player.js
@@ -10,8 +10,8 @@ var Player = function(game, x, y, width, height) {
 	this.angle = 0;
 	this.maxJumpDist = 12;
 	this.jumpDist = 0;
-	this.lasty = y;
-	this.lastx = x;
+	this.lastY = y;
+	this.lastX = x;
     this.DEBUG = {};
 	
 	// States
@@ -93,83 +93,106 @@ Player.prototype.update = function(game) {
 	this.velocityY *= 0.90;
 
 	// Collision point
-	var adjx = ((this.width / 2) * Math.cos((this.angle + 90) * Math.PI / 180)) + this.x;
-	var adjy = ((this.width / 2) * Math.sin((this.angle + 90) * Math.PI / 180)) + (this.y + 2); // 2px padding for stroke width
+	var adjX = ((this.width / 2) * Math.cos((this.angle + 90) * Math.PI / 180)) + this.x;
+	var adjY = ((this.width / 2) * Math.sin((this.angle + 90) * Math.PI / 180)) + (this.y + 2); // 2px padding for stroke width
+    
+	// Finds the line the player is going to collide with
+	var playerPos = adjX / /*levelPrefs.width*/30 >> 0;
 	
+	// Collide with tiles
+    var tile0 = game.sceneManager.scene.tiles[playerPos - 1];
+    var tile1 = game.sceneManager.scene.tiles[playerPos];
+    var tile2 = game.sceneManager.scene.tiles[playerPos + 1];
+    
+    var run = tile1.x2 - tile1.x1;
+    var rise = tile1.y2 - tile1.y1;
+    var slope = rise / run;
+    var intersect = tile1.y1 - (tile1.x1 * slope);
+    var col = (slope * adjX) + intersect;
+    
+    var angle = Util.radToDeg(Math.atan(rise / run));
+    this.angle = angle;
+    
+    // Collide with current tile's y
+    if(adjY > col && tile1.type != TileType.air) {
+        if(this.lastY < col) {
+            if(game.inputManager.isKeyDown(KeyAction.down) && tile1.type == TileType.passthrough) {
+                //this.isFalling = true;
+            } else {
+                this.y = col + (this.y - adjY);
+                this.isFalling = false;
+            }
+            
+            // Speed up while going down a slope and holding the down key
+            if(this.isOnGround && game.inputManager.isKeyDown(KeyAction.down) && tile1.type == TileType.solid) {
+                var pullVel = 0.4;
+                if(this.velocityX > 0 && this.angle > 0 && this.angle <= 45) {
+                    var vel = this.angle / 25 * 0.3 + 0.1;
+                    if(vel > pullVel) { vel = pullVel; }
+                    if(vel < 0) { vel = 0; }
+                    this.velocityX += vel;
+                }
+                /*if(this.velocityX < 0 && this.angle < 0 && this.angle >= -45) {
+                    var vel = (this.angle * -1) / 45 * pullVel;
+                    if(vel > pullVel) { vel = pullVel; }
+                    if(vel < 0) { vel = 0; }
+                    this.velocityX -= vel;
+                }*/
+            }
+        }
+    } else {
+        if(!this.isJumping) {
+            this.isFalling = true;
+        }
+        
+        // Collide with left tile's y
+        if(tile0.type != TileType.air
+        && this.lastY < tile0.y2 && adjY > tile0.y2 
+        && adjX - this.width / 2 < tile0.x2) {
+            if(game.inputManager.isKeyDown(KeyAction.down) && tile0.type == TileType.passthrough) {
+                //this.isFalling = true;
+            } else {
+                this.y = (tile0.y2 * 1) + (this.y - adjY);
+                this.isFalling = false;
+            }
+        }
+        
+        // Collide with right tile's y
+        if(tile2.type != TileType.air 
+        && this.lastY < tile2.y1 && adjY > tile2.y1 
+        && adjX + this.width / 2 > tile2.x1) {
+            if(game.inputManager.isKeyDown(KeyAction.down) && tile2.type == TileType.passthrough) {
+                //this.isFalling = true;
+            } else {
+                this.y = (tile2.y1 * 1) + (this.y - adjY);
+                this.isFalling = false;
+            }
+        }
+    }
+    
+    // Collide with left tile's x
+    if(tile0.type == TileType.solid && adjX - this.width / 2 < tile0.x2 && adjY > tile0.y2) {
+        if(this.lastY > tile0.y2) {
+            this.x = tile0.x2 + this.width / 2;
+        }
+        //this.isAlive = false;
+    }
+    
+    // Collide with right tile's x
+    if(tile2.type == TileType.solid && adjX + this.width / 2 > tile2.x1 && adjY > tile2.y1) {
+        if(this.lastY > tile2.y1) {
+            this.x = tile2.x1 - this.width / 2;
+        }
+        //this.isAlive = false;
+    }
+ 	
 	// Find the y direction the player is moving
 	var yvel;
 	var yfixed = this.y.toFixed(1);
-	if(yfixed < this.lasty) yvel = 1;
-	if(yfixed > this.lasty) yvel = -1;
-	if(yfixed == this.lasty) yvel = 0;
+	if(yfixed < this.lastY) yvel = 1;
+	if(yfixed > this.lastY) yvel = -1;
+	if(yfixed == this.lastY) yvel = 0;
     
-	// Finds the line the player is going to collide with
-	var playerPos = adjx / /*levelPrefs.width*/30 >> 0;
-	
-	// Loop through tiles and collide with the player
-	for(var i = 0; i < game.sceneManager.scene.tiles.length; i++) {
-		if(i === playerPos) {
-            var tile0 = game.sceneManager.scene.tiles[i-1];
-            var tile1 = game.sceneManager.scene.tiles[i];
-            var tile2 = game.sceneManager.scene.tiles[i+1];
-            
-            var run = tile1.x2 - tile1.x1;
-            var rise = tile1.y2 - tile1.y1;
-            var slope = rise / run;
-            var intersect = tile1.y1 - (tile1.x1 * slope);
-            var col = (slope * adjx) + intersect;
-            
-            var angle = Util.radToDeg(Math.atan(rise / run));
-            this.angle = angle;
-            
-            // Collide with current tile's y
-            if(adjy > col && tile1.type != TileType.air) {
-                if(this.lasty < col && !(game.inputManager.isKeyDown(KeyAction.down) && tile1.type == TileType.passthrough)) {
-                    this.y = col + (this.y - adjy);
-                    this.isFalling = false;
-                }
-            } else {
-                // Collide with left tile's y
-                if(tile0.type != TileType.air 
-                && this.lasty < tile0.y2 && adjy > tile0.y2 
-                && adjx - this.width / 2 < tile0.x2
-                && !(game.inputManager.isKeyDown(KeyAction.down) && tile0.type == TileType.passthrough)) {
-                    this.y = (tile0.y2 * 1) + (this.y - adjy);
-                    this.isFalling = false;
-                }
-                
-                // Collide with right tile's y
-                if(tile2.type != TileType.air 
-                && this.lasty < tile2.y1 && adjy > tile2.y1 
-                && adjx + this.width / 2 > tile2.x1
-                && !(game.inputManager.isKeyDown(KeyAction.down) && tile2.type == TileType.passthrough)) {
-                    this.y = (tile2.y1 * 1) + (this.y - adjy);
-                    this.isFalling = false;
-                }
-                
-                if(!this.isJumping) {
-                    this.isFalling = true;
-                }
-            }
-            
-            // Collide with left tile's x
-            if(tile0.type == TileType.solid && adjx - this.width / 2 < tile0.x2 && adjy > tile0.y2) {
-                if(this.lasty > tile0.y2) {
-                    this.x = tile0.x2 + this.width / 2;
-                }
-                //this.isAlive = false;
-            }
-            
-            // Collide with right tile's x
-            if(tile2.type == TileType.solid && adjx + this.width / 2 > tile2.x1 && adjy > tile2.y1) {
-                if(this.lasty > tile2.y1) {
-                    this.x = tile2.x1 - this.width / 2;
-                }
-                //this.isAlive = false;
-            }
-        }
-	}
-	
 	// Apply extra gravity to keep player on slope during downward movement
 	// This prevents a bug where the player cannot jump while moving quickly
 	// downwards due to being in a "falling" state.
@@ -196,7 +219,7 @@ Player.prototype.update = function(game) {
     }
 	
 	// Die when falling off the edge of the world
-	if(adjy > game.renderManager.canvas.height + this.width) {
+	if(adjY > game.renderManager.canvas.height + this.width) {
 		this.isAlive = false;
 	}
 	
@@ -224,8 +247,8 @@ Player.prototype.update = function(game) {
 		this.isAlive = true;
 	}
 	
-	this.lasty = this.y.toFixed(1); // last known y position
-	this.lastx = this.x.toFixed(1); // last known y position
+	this.lastY = this.y.toFixed(1); // last known y position
+	this.lastX = this.x.toFixed(1); // last known y position
 	
 	// Update the camera position
 	game.sceneManager.scene.camera.x = this.x - (game.renderManager.canvas.width / 2);
@@ -236,5 +259,5 @@ Player.prototype.draw = function(renderManager, camera) {
 	var y = ((this.width / 2) * Math.sin((this.angle + 90) * Math.PI / 180)) + this.y;
 	renderManager.drawLine(this.x - camera.x, this.y, x - camera.x, y, "#a7c6e0", 2);
 	//renderManager.drawRectangle((this.x - this.width / 2) - camera.x, this.y - this.height / 2, this.width, this.height, "#114A93", 2, "transparent");
-	renderManager.drawCircle(this.x - camera.x, this.y, this.width / 2, "#218ae0", 2, "transparent");
+	renderManager.drawCircle(this.x - camera.x, this.y, this.width / 2, this.isFalling ? "red" : "#218ae0", 2, "transparent");
 };
